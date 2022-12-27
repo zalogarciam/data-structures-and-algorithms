256e1a46d9832a2830a42359f46195c144c70186
zalogarciam <gegarciam95@gmail.com>
2022-12-27 16:25:09 -0500

    Duplicate size of array feature

--- _array.py
+++ _array.py
@@ -11,24 +11,35 @@ class MyArray():
         self.items = []
 
     def insert(self, item):
-        self.items.append(item)
+        if (self.size > len(self.items)):
+            self.items.append(item)
+        else:
+            print("Capacity is not enough, duplicating array size")
+            self.size = self.size * 2
+            self.items.append(item)
+            for i in range(len(self.items), self.size):
+                self.items.append(None)
+
 
     def removeAt(self, index):
         self.items.remove(self.items[index])
 
     def indexOf(self, item):
-        for i in range(len(self.items)):
+        for i in range(self.size):
             if self.items[i] == item:
+                print(i)
                 return i
+        return -1
 
     def printItems(self):
-        for i in range(len(self.items)):
-            print(self.items[i])
+        for i in range(self.size):
+            print("Item "+ str(i) + ":" + str(self.items[i]))
+        print(self.items)
 
 myArray = MyArray(3)
 myArray.insert(10)
 myArray.insert(20)
 myArray.insert(30)
 myArray.insert(40)
-myArray.indexOf(3)
+myArray.indexOf(40)
 myArray.printItems()
