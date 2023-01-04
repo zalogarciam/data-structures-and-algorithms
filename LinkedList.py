c63c08e6b8c876215c1e10bb030a61ce210cdf13
zalogarciam <gegarciam95@gmail.com>
2023-01-03 20:04:08 -0500

    Implementation of add first and last

--- LinkedList.py
+++ LinkedList.py
@@ -13,6 +13,7 @@ class LinkedList():
     def __init__(self):
         self.head = None
         self.tail = None
+        self.index = 0
 
     def print_list(self):
         current = self.head
@@ -20,9 +21,23 @@ class LinkedList():
             print(current.value)
             current = current.next
 
+    def add_first(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            self.index += 1
+        else:
+            print("Head already added")
+
+    def add_last(self, value):
+        if self.tail is None:
+            self.tail = Node(value)
+            self.head.next = self.tail
+            self.index += 1
+        else:
+            print("Tail already added")
+
 
 linkedList = LinkedList()
-linkedList.head = Node(5)
-linkedList.tail = Node(6)
-linkedList.head.next = linkedList.tail
+linkedList.add_first(1)
+linkedList.add_last(2)
 linkedList.print_list()
