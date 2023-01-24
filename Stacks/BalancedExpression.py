dd16f6679025edcc3a0cef94c9a6408114dd0ca1
zalogarciam <gegarciam95@gmail.com>
2023-01-24 15:00:07 -0500

    Fix bug when string only has openers

--- Stacks/BalancedExpression.py
+++ Stacks/BalancedExpression.py
@@ -9,9 +9,12 @@ def balanced_expression(string):
             open_character = stack.pop()
             if open_list.index(open_character) != close_list.index(character):
                 return False
+    if (len(stack) > 0):
+        return False
     return True
 
 
-
+print(balanced_expression("(1 + 2)"))
+print(balanced_expression("(1 + 2"))
 print(balanced_expression("(([1] + <2>))[a]"))
 print(balanced_expression("((<1] + <2>))[a]"))
