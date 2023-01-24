7e728ee73bd1f05586c89a0fd6251b0e4bb57828
zalogarciam <gegarciam95@gmail.com>
2023-01-24 15:14:22 -0500

    Adding more special test cases

--- Stacks/BalancedExpression.py
+++ Stacks/BalancedExpression.py
@@ -6,13 +6,16 @@ def balanced_expression(string):
         if character in open_list:
             stack.append(character)
         if character in close_list:
-            if (len(stack) == 0): return False
+            if len(stack) == 0: return False
             open_character = stack.pop()
             if open_list.index(open_character) != close_list.index(character):
                 return False
-    if (len(stack) > 0): return False
+    if len(stack) > 0: return False
     return True
 
+print(balanced_expression("1 + 2"))
+print(balanced_expression(""))
+print(balanced_expression("("))
 print(balanced_expression("1 + 2)))"))
 print(balanced_expression("(1 + 2)))"))
 print(balanced_expression("(1 + 2)"))
