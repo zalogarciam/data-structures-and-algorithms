df5765a2c711ebe679f1efd6fc0e660b19f5fa5d
zalogarciam <gegarciam95@gmail.com>
2023-01-24 15:38:12 -0500

    Import array and create structure of stack methods

--- Stacks/Stack.py
+++ Stacks/Stack.py
@@ -1,2 +1,17 @@
-class Stack():
-    pass
+from Arrays.MyArray import MyArray
+
+class Stack(MyArray):
+    def __init__(self):
+        pass
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+    
+    def is_empty(self):
+        pass
