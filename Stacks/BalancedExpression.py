04fd11f14d73389742198b75040200bf69206406
zalogarciam <gegarciam95@gmail.com>
2023-01-24 15:06:41 -0500

    Covering case when only closers

--- Stacks/BalancedExpression.py
+++ Stacks/BalancedExpression.py
@@ -6,14 +6,15 @@ def balanced_expression(string):
         if character in open_list:
             stack.append(character)
         if character in close_list:
+            if (len(stack) == 0): return False
             open_character = stack.pop()
             if open_list.index(open_character) != close_list.index(character):
                 return False
-    if (len(stack) > 0):
-        return False
+    if (len(stack) > 0): return False
     return True
 
-
+print(balanced_expression("1 + 2)))"))
+print(balanced_expression("(1 + 2)))"))
 print(balanced_expression("(1 + 2)"))
 print(balanced_expression("(1 + 2"))
 print(balanced_expression("(([1] + <2>))[a]"))
