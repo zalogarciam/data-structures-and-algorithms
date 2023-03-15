8e3881c45359d8a231dd8e84babbe3de55e3b2f8
zalogarciam <gegarciam95@gmail.com>
2023-03-14 21:11:32 -0500

    Implement queue based on a linked list

---
+++ Queues/LinkedListQueue.py
@@ -0,0 +1,192 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.index = 0
+
+    def print_list(self):
+        current = self.head
+        linked_list = ""
+        while(current is not None):
+            if current.next is None:
+                linked_list += str(current.value)
+            else:
+                linked_list += str(current.value) + "=>"
+            current = current.next
+        print(linked_list)
+
+    def add_first(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:  
+            new_node.next = self.head
+            self.head = new_node
+        self.index += 1
+
+    def add_last(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.index += 1
+
+    def delete_first(self):
+        if self.is_empty():
+            raise Exception("LinkedList is empty")
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            second = self.head.next
+            self.head.next = None
+            self.head = second
+        self.index -= 1
+
+    def delete_last(self):
+        last = None
+        if self.is_empty():
+            raise Exception("LinkedList is empty")
+        if self.head == self.tail:
+            last = self.head
+            self.head = None
+            self.tail = None
+        else:     
+            last = self.tail
+            previous = self.get_previous(self.tail)
+            self.tail = previous   
+            previous.next = None     
+        self.index -= 1
+        return last.value
+
+    def contains(self, value):
+        return self.index_of(value) != -1
+
+    def index_of(self, value):
+        count = 0
+        current = self.head
+        while (current is not None):
+            if current.value == value:
+                return count
+            current = current.next
+            count += 1
+        return -1
+
+    def is_empty(self):
+        return self.head is None
+
+    def get_previous(self, node):
+        current = self.head
+        while current is not None:
+            if current.next == node: return current
+            current = current.next
+        return None
+
+    def size(self):
+        return self.index
+
+    def to_array(self):
+        array = []
+        current = self.head
+        while (current is not None):
+            array.append(current.value)
+            current = current.next
+        return array
+
+    def reverse(self):
+        if self.is_empty(): return
+
+        previous = self.head
+        current = self.head.next
+        while current is not None:
+            next = current.next
+            current.next = previous
+            current = next
+            previous = current
+
+        self.tail = self.head
+        self.tail.next = None
+        self.head = previous
+        
+    def get_kth_from_the_end(self, k):
+        if k <= 0 or self.is_empty(): return
+        if k > self.size(): return 
+        
+        first = self.head
+        second = self.head
+        for i in range(k):
+            second = second.next
+        while second is not None:
+            first = first.next
+            second = second.next
+        return first.value
+    
+    def print_middle(self):
+        if self.is_empty(): return
+        if self.head.next is None: return self.head.value
+
+        first = self.head
+        second = self.head
+
+        while second != self.tail and second.next != self.tail:
+            second = second.next.next
+            first = first.next
+        if second == self.tail:
+            return first.value
+        else:
+            return (first.value, first.next.value)
+
+    def has_loop(self):
+        if self.is_empty(): return
+        if self.head.next is None: return self.head.value
+
+        first = self.head
+        second = self.head
+        while second.next.next is not None:
+            second = second.next.next
+            first = first.next
+            if second.next is None:
+                return False
+            if second.value == first.value:
+                return True
+        return False
+    
+class LinkedListQueue(LinkedList):
+    def __init__(self):
+        super().__init__()
+
+    def add(self, value):
+        self.add_last(value)
+
+    def remove(self):
+        return self.delete_first()
+
+    def peek(self):
+        return self.head.value
+
+    def is_empty(self):
+        return self.index == 0
+
+    def print_queue(self):
+        self.printItems()
+
+
+queue = LinkedListQueue()
+queue.add(2)
+queue.add(4)
+queue.add(5)
+queue.print_list()
+queue.remove()
+queue.add(1)
+print(queue.peek())
+print(queue.is_empty())
+queue.print_list()
