1215fae5ae0c5ccbea389a3dd6b3f91fc1dcd9a9
zalogarciam <gegarciam95@gmail.com>
2023-03-21 11:27:31 -0500

    Reverse queue exercise

--- Queues/ArrayQueue.py
+++ Queues/ArrayQueue.py
@@ -19,12 +19,14 @@ class MyArray():
         if index < 0 or index > self.size - 1:
             raise Exception("Index out of range")
         else:
+            removed_item = self.items[index]
             for i in range(index, self.size):
                 if i + 1 < self.size:
                     self.items[i] = self.items[i + 1]
                 else:
                     self.items[i] = None
             self.index -= 1
+            return removed_item
 
     def indexOf(self, item):
         for i in range(self.index):
@@ -38,7 +40,8 @@ class MyArray():
 
     def printItems(self):
         for i in range(self.index):
-            print("Item "+ str(i) + ": " + str(self.items[i]))
+            if (self.items[i] != None):
+                print("Item "+ str(i) + ": " + str(self.items[i]))
 
     def max(self):
         max = -1
@@ -75,6 +78,25 @@ class MyArray():
             self.items[i] = self.items[i - 1]
         self.items[index] = item
 
+class ArrayStack(MyArray):
+    def __init__(self, size):
+        super().__init__(size)
+
+    def push(self, value):
+        self.insert(value)
+
+    def pop(self):
+        return self.removeAt(self.index - 1)
+
+    def peek(self):
+        return self.items[self.index - 1]
+
+    def is_empty(self):
+        return self.index == 0
+
+    def print_stack(self):
+        self.printItems()
+
 class ArrayQueue(MyArray):
     def __init__(self, size):
         super().__init__(size)
@@ -94,6 +116,25 @@ class ArrayQueue(MyArray):
     def print_queue(self):
         self.printItems()
 
+    def reverse_queue(self):
+        reversed_queue = []
+        for i in range(len(self.items) - 1, -1, -1):
+            if (self.items[i] is not None):
+                reversed_queue.append(self.items[i])
+        return reversed_queue
+    
+    def reverse_queue_2(self, size):
+        stack = ArrayStack(size)
+        for i in range(0, size - 1):
+            if self.items[i] != None:
+                stack.insert(self.items[i])
+        while stack.is_empty() == False:
+            item = stack.pop()
+            if item is not None:
+                print(item, end='-')
+
+ 
+
 queue = ArrayQueue(10)
 queue.add(2)
 queue.add(4)
@@ -102,4 +143,6 @@ queue.remove()
 queue.add(1)
 queue.peek()
 queue.is_empty()
-queue.print_queue()
+# queue.print_queue()
+# print(queue.reverse_queue())
+print(queue.reverse_queue_2(10))
