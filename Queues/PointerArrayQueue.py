c03e7c93b35782eef661f857881d45348c650107
zalogarciam <gegarciam95@gmail.com>
2023-03-21 11:58:04 -0500

    Implemenet pointer array queue

---
+++ Queues/PointerArrayQueue.py
@@ -0,0 +1,41 @@
+class PointerArrayQueue():
+    def __init__(self, size) -> None:
+        self.size = size
+        self.items = [None] * size
+        self.index = 0
+        self.front = 0
+        self.back = 0
+
+    def enqueue(self, data):
+        if self.index == self.size:
+            raise Exception("Queue is full")
+        self.items[self.index] = data
+        self.back += 1
+        self.index += 1
+
+    def dequeue(self):
+        self.front += 1
+        self.index -= 1
+
+    def is_empty(self):
+        return self.index == 0
+
+    def is_full(self):
+        return self.index == self.size
+
+    def print_queue(self):
+        for i in range(self.front, self.back):
+            print(self.items[i])
+
+queue = PointerArrayQueue(10)
+queue.enqueue(1)
+queue.enqueue(3)
+queue.enqueue(5)
+queue.enqueue(10)
+queue.enqueue(10)
+queue.enqueue(10)
+queue.enqueue(10)
+queue.enqueue(10)
+queue.enqueue(10)
+queue.dequeue()
+queue.print_queue()
