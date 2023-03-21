15b2ef636b313e05e5d78bbc8a467690cb84d963
zalogarciam <gegarciam95@gmail.com>
2023-03-21 13:35:17 -0500

    Module to become a cirular array

--- Queues/PointerArrayQueue.py
+++ Queues/PointerArrayQueue.py
@@ -2,44 +2,43 @@ class PointerArrayQueue():
     def __init__(self, size) -> None:
         self.size = size
         self.items = [None] * size
-        self.index = 0
+        self.count = 0
         self.front = 0
         self.back = 0
 
     def enqueue(self, data):
-        if self.index == self.size:
+        if self.count == self.size:
             raise Exception("Queue is full")
-        self.items[self.index] = data
-        self.back += 1
-        self.index += 1
+        print("Index: " + str(self.count))
+        self.items[self.back] = data
+        self.back = (self.back + 1) % self.size
+        self.count += 1
 
     def dequeue(self):
         item = self.items[self.front]
         self.items[self.front] = None
-        self.front += 1
-        self.index -= 1
+        self.front = (self.front + 1) % self.size
+        self.count -= 1
         return item
 
     def is_empty(self):
-        return self.index == 0
+        return self.count == 0
 
     def is_full(self):
-        return self.index == self.size
+        return self.count == self.size
 
     def print_queue(self):
-        for i in range(self.front, self.back):
+        for i in range(self.size):
             print(self.items[i])
 
-queue = PointerArrayQueue(10)
+queue = PointerArrayQueue(5)
 queue.enqueue(1)
 queue.enqueue(3)
 queue.enqueue(5)
-queue.enqueue(10)
-queue.enqueue(10)
-queue.enqueue(10)
-queue.enqueue(10)
-queue.enqueue(10)
+queue.enqueue(7)
 queue.enqueue(10)
 queue.dequeue()
-queue.enqueue(10)
+queue.dequeue()
+queue.enqueue(12)
+queue.enqueue(14)
 queue.print_queue()
