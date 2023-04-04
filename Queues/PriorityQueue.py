45e48121d994dde878d36ea3970106ed42ec71b1
zalogarciam <gegarciam95@gmail.com>
2023-04-04 16:43:20 -0500

    Handle queue when size is full

--- Queues/PriorityQueue.py
+++ Queues/PriorityQueue.py
@@ -6,7 +6,23 @@ class PriorityQueueArray():
         self.count = 0
 
     def insert(self, data):
-        pass
+        if self.is_empty():
+            self.items[self.count] = data
+        else:
+            if self.count == self.size:
+                new_items = [None] * (self.size * 2)
+                for i in range(0, self.size):
+                    new_items[i] = self.items[i]
+                self.size = self.size * 2
+                self.items = new_items
+            for i in range(self.count - 1, -1, -1):
+                if data < self.items[i]:
+                    self.items[i + 1] = self.items[i]
+                    self.items[i] = data
+                else:
+                    self.items[i + 1] = data
+                    break
+        self.count += 1
 
     def is_empty(self):
         return self.count == 0
@@ -17,11 +33,17 @@ class PriorityQueueArray():
     def print(self):
         for item in self.items:
             print(item, end=" - ")
-
+        print("\n")
 
 class PriorityQueue(PriorityQueueArray):
     def __init__(self, size) -> None:
         super().__init__(size)
 
 queue = PriorityQueue(5)
+queue.insert(3)
+queue.insert(2)
+queue.insert(5)
+queue.insert(7)
+queue.insert(1)
+queue.insert(9)
 queue.print()
