672a8ed344603c93b6b303421634492ca57ce730
zalogarciam <gegarciam95@gmail.com>
2023-04-04 12:02:40 -0500

    Is empty and is full methods

--- Queues/PriorityQueue.py
+++ Queues/PriorityQueue.py
@@ -3,6 +3,17 @@ class PriorityQueueArray():
     def __init__(self, size) -> None:
         self.size = size
         self.items = [None] * size
+        self.count = 0
+
+    def insert(self, data):
+        pass
+
+    def is_empty(self):
+        return self.count == 0
+    
+    def is_full(self):
+        return self.size == self.count
+
     def print(self):
         for item in self.items:
             print(item, end=" - ")
