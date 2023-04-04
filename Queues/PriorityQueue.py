9f23573ee508bf88b8dfba567ba4ab2781df9d57
zalogarciam <gegarciam95@gmail.com>
2023-04-04 11:54:34 -0500

    Add structure for array and priority queue

--- Queues/PriorityQueue.py
+++ Queues/PriorityQueue.py
@@ -1,2 +1,16 @@
-class PriorityQueue():
-    pass
+
+class PriorityQueueArray():
+    def __init__(self, size) -> None:
+        self.size = size
+        self.items = [None] * size
+    def print(self):
+        for item in self.items:
+            print(item, end=" - ")
+
+
+class PriorityQueue(PriorityQueueArray):
+    def __init__(self, size) -> None:
+        super().__init__(size)
+
+queue = PriorityQueue(5)
+queue.print()
