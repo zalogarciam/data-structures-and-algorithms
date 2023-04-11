b4bd8970a5b59fcc209c65da1718d0315fe8cb0b
zalogarciam <gegarciam95@gmail.com>
2023-04-11 16:12:49 -0500

    Testing sets

--- HashTables/HashTable.py
+++ HashTables/HashTable.py
@@ -14,3 +14,6 @@ def find_first_non_repated_character(string):
             return i
             
 print(find_first_non_repated_character('a green apple'))
+
+my_set = set((1, 1, 2 ,3 ,4, 5 , 6))
+print(my_set)
