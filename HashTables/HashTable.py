955461b97e0e682028ee078bcf02643bc6ee1cfa
zalogarciam <gegarciam95@gmail.com>
2023-04-11 16:20:23 -0500

    Change dict to set

--- HashTables/HashTable.py
+++ HashTables/HashTable.py
@@ -1,7 +1,7 @@
 class HashTable():
     pass
 
-def find_first_non_repated_character(string):
+def find_first_non_repeated_character(string):
     dict = {}
     for i in string:
         if i != ' ':
@@ -13,7 +13,19 @@ def find_first_non_repated_character(string):
         if dict[i] == 1:
             return i
             
-print(find_first_non_repated_character('a green apple'))
+print(find_first_non_repeated_character('a green apple'))
 
 my_set = set((1, 1, 2 ,3 ,4, 5 , 6))
 print(my_set)
+print(1 in my_set)
+
+def first_first_repeated_character(string):
+    dict = set()
+    for i in string:
+        if i != ' ':
+            if i not in dict:
+                dict.add(i)
+            else:
+                return i
+
+print(first_first_repeated_character('green apple'))
