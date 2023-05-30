6fd24b4ec25969ad09d710c0d531daa7e99dd186
zalogarciam <gegarciam95@gmail.com>
2023-05-30 14:57:46 -0500

    Added find item in tree

--- BinaryTrees/BinaryTree.py
+++ BinaryTrees/BinaryTree.py
@@ -5,20 +5,28 @@ class Node():
         self.right = None
 
     def insert(self, data):
-            if data is None:
-                self.data = data
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data)
             else:
-                if data < self.data:
-                    if self.left is None:
-                        self.left = Node(data)
-                    else:
-                        self.left.insert(data)
-                else:
-                    if self.right is None:
-                        self.right = Node(data)
-                    else:
-                        self.right.insert(data)
+                self.left.insert(data)
+        else:
+            if self.right is None:
+                self.right = Node(data)
+            else:
+                self.right.insert(data)
 
+    def find(self, data):
+        if data == self.data:
+            return True
+        if data < self.data:
+            if self.left is not None:
+                return self.left.find(data)
+        else:
+            if self.right is not None:
+                return self.right.find(data)
+        return False
+    
     def print(self, node, level = 0):
         if node.right is not None: self.print(node.right, level + 1)
         print(' ' * 4 * level + '-> ' + str(node.data))
@@ -34,5 +42,8 @@ root.insert(8)
 root.insert(12)
 root.insert(18)
 root.insert(17)
+print(root.find(6))
+print(root.find(7))
+
 root.print(root)
 
