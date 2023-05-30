46c2c7a7fae12a5c58b71b578f453ee6d5464bf3
zalogarciam <gegarciam95@gmail.com>
2023-05-30 09:31:53 -0500

    Adding trees folder

---
+++ BinaryTrees/BinaryTree.py
@@ -0,0 +1,12 @@
+class Node():
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self) -> None:
+        pass
+
+    def insert(self):
+        pass
