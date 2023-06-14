a27343ef476acc1e982bc874c3115988c71e155e
zalogarciam <gegarciam95@gmail.com>
2023-06-14 13:50:00 -0500

    Added equals method to compare to trees

--- BinaryTrees/BinaryTree.py
+++ BinaryTrees/BinaryTree.py
@@ -81,6 +81,11 @@ class Tree():
             current = current.left
         return last.data
 
+    def equals(self, node1, node2):
+        if node1 is None and node2 is None: return True
+        if node1 is not None and node2 is not None:
+            return node1.data == node2.data and self.equals(node1.left, node2.left) and self.equals(node1.right, node2.right)
+        return False
 
 tree = Tree()
 tree.insert(10)
@@ -92,6 +97,7 @@ tree.insert(8)
 tree.insert(12)
 tree.insert(18)
 tree.insert(17)
+
 print(tree.find(6))
 print(tree.find(7))
 
@@ -102,9 +108,21 @@ print('Height: ', tree.height(tree.root))
 print('Min: ', tree.min_value(tree.root))
 print('Min BST: ', tree.min_bst(tree.root))
 
-
 #Exercise
 # BFS: 20 10 30 6 14 24 3 8 26
 # PRE (ROOT, LEFT, RIGHT): 20 10 6 3 8 14 30 24 26
 # IN (LEFT, ROOT, RIGHT):  3 6 8 10 14 20 24 26 30
 # POS (LEFT, RIGHT, ROOT): 3 8 6 14 10 26 24 30 20
+
+tree2 = Tree()
+tree2.insert(10)
+tree2.insert(5)
+tree2.insert(15)
+tree2.insert(6)
+tree2.insert(1)
+tree2.insert(8)
+tree2.insert(12)
+tree2.insert(18)
+tree2.insert(17)
+
+print(tree2.equals(tree.root, tree2.root))
