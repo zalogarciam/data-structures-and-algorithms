c6924387d3d88755e7de68a20a772f2523275bc3
zalogarciam <gegarciam95@gmail.com>
2023-07-04 18:34:55 -0500

    Update remove method

--- Heaps/Heap.py
+++ Heaps/Heap.py
@@ -10,32 +10,78 @@ class Heap:
         count = self.size
         index_parent = int((self.size/ 2) - 1)
         # Bubble up
-        while True:
+        while self.items[index_parent] < value:
             parent = self.items[index_parent]
-            if parent < value:
-                self.items[count - 1] = parent
-                self.items[index_parent] = value
-                index_parent = int(((index_parent + 1)/ 2) - 1)
-                count = int(count / 2)
-            else:
-                break
+            self.items[count - 1] = parent
+            self.items[index_parent] = value
+            index_parent = int(((index_parent + 1)/ 2) - 1)
+            count = int(count / 2)
+
+    def insert_(self, value):
+        self.items.append(value)
+        self.size += 1
+        self.bubble_up()
         
+    def bubble_up(self):
+        index = self.size - 1
+        while index > 0 and self.items[index] > self.items[self.parent(index)]:
+            self.swap(index, self.parent(index))
+            index = self.parent(index)
+    
+    def parent(self, index):
+        return math.ceil((index - 1)/2)
+
+    def swap(self, first, second):
+        temp = self.items[first]
+        self.items[first] = self.items[second]
+        self.items[second] = temp
+
+    def remove_(self):
+        if self.size == 0: raise Exception("Empty heap")
+        self.items[0] = self.items[-1]
+        del self.items[-1]
+        self.bubble_down()       
+        self.size -= 1
+
+    def bubble_down(self):
+        index = 0
+        while index <= self.size and not self.is_valid_parent(index):
+            larger_child_index = self.larger_child_index(index)
+            self.swap(index, larger_child_index)
+            index = self.larger_child_index(index)
+
+    def larger_child_index(self, index):
+        return self.left_child_index(index) if self.left_child(index) > self.right_child(index) else self.right_child_index(index) 
+
+    def is_valid_parent(self, index):
+        return self.items[index] >= self.left_child(index) and self.items[index] >= self.right_child(index)
+
+    def left_child(self, index):
+        return self.items[self.left_child_index(index)]
+    
+    def right_child(self, index):
+        return self.items[self.right_child_index(index)]
+
+    def left_child_index(self, index):
+        return index * 2 +  1
+    
+    def right_child_index(self, index):
+        return index * 2 +  2
+
     def remove(self):
         self.items[0] = self.items[-1]
         del self.items[-1]
+        self.size -= 1
         count = 0
         child_index = (self.items.index(self.items[count]) * 2) + 1
         # Bubble down
-        while True:
+        while self.items[count] < self.items[child_index]:
             child = self.items[child_index]
             current = self.items[count]
-            if current < child:
-                self.items[count] = child
-                self.items[child_index] = current
-                child_index = (child_index * 2) + 1
-                count = (count * 2) + 1
-            else:
-                break
+            self.items[count] = child
+            self.items[child_index] = current
+            child_index = (child_index * 2) + 1
+            count = (count * 2) + 1
             
     def print_heap(self):
         height = math.ceil((math.log(self.size + 1,2)))
@@ -50,16 +96,16 @@ class Heap:
         print(self.items)
 
 heap = Heap()
-heap.insert(15)
-heap.insert(10)
-heap.insert(3)
-heap.insert(8)
-heap.insert(12)
-heap.insert(9)
-heap.insert(4)
-heap.insert(1)
-heap.insert(24)
+heap.insert_(15)
+heap.insert_(10)
+heap.insert_(3)
+heap.insert_(8)
+heap.insert_(12)
+heap.insert_(9)
+heap.insert_(4)
+heap.insert_(1)
+heap.insert_(24)
 heap.print_heap()
-heap.remove()
+heap.remove_()
 heap.print_heap()
 
