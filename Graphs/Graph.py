d248d6a473a41763d6d10ea5117af576a52965d1
zalogarciam <gegarciam95@gmail.com>
2023-08-07 15:37:27 -0500

    Improve implementation with dict instead of list

--- Graphs/Graph.py
+++ Graphs/Graph.py
@@ -2,47 +2,42 @@
 class Graph:
     def __init__(self) -> None:
         self.nodes = {}
+        self.adjacency_list = {}
 
     class Node:
         def __init__(self, label) -> None:
             self.label = label
-            self.neighbors = []
-
-        def has_neighbors(self):
-            return len(self.neighbors) > 0
-        
-        def neighbor_exists(self, node):
-            return node in self.neighbors
 
     def add_node(self, label):
         if label not in self.nodes:
             self.nodes[label] = self.Node(label)
+            self.adjacency_list[label] = []
 
     def remove_node(self, label):
         if label not in self.nodes:
             return
-        for node in self.nodes:
-            current = self.nodes[node]
-            if current.has_neighbors() and current.neighbor_exists(self.nodes[label]):
-                self.remove_edge(node, label)
+        node = self.nodes[label]
+        for key in self.adjacency_list:
+            if node in self.adjacency_list[key]:
+                self.adjacency_list[key].remove(node)
+
+        del self.adjacency_list[label]
         del self.nodes[label]
 
     def add_edge(self, source, destination):
         if source not in self.nodes or destination not in self.nodes:
             raise Exception('Node does not exist')
-        if destination not in self.nodes[source].neighbors:
-            self.nodes[source].neighbors.append(self.nodes[destination])
+        self.adjacency_list[source].append(self.nodes[destination])
 
     def remove_edge(self, source, destination):
         if source not in self.nodes or destination not in self.nodes:
             return
-        if self.nodes[destination] in self.nodes[source].neighbors:
-            self.nodes[source].neighbors.remove(self.nodes[destination])
+        self.adjacency_list[source].remove(self.nodes[destination])
 
     def print_graph(self):
         for node in self.nodes:
             current = self.nodes[node]
-            for neighbor in current.neighbors:
+            for neighbor in self.adjacency_list[current.label]:
                 print(self.nodes[node].label, "-->", neighbor.label)
         print()
 # Improvement - change neighbors list to dict
@@ -54,7 +49,7 @@ graph.add_edge('A', 'B')
 graph.add_edge('A', 'C')
 graph.add_edge('B', 'A')
 graph.print_graph()
-graph.remove_node('C')
+graph.remove_node('A')
 # graph.remove_edge('A', 'C')
 
 graph.print_graph()
