05b0d931f49b595ba34f237a20be83b06bf48479
zalogarciam <gegarciam95@gmail.com>
2023-08-16 20:31:11 -0500

    Implementation of weighted graph

---
+++ Graphs/WeightedGraph.py
@@ -0,0 +1,48 @@
+class WeightedGraph:
+    def __init__(self) -> None:
+        self.nodes = {}
+        self.adjacency_list = {}
+
+    class Node:
+        def __init__(self, label) -> None:
+            self.label = label
+
+    class Edge:
+        def __init__(self, source, destination, weight) -> None:
+            self.source = source
+            self.destination = destination
+            self.weight = weight
+    
+    def add_node(self, label):
+        if label not in self.nodes:
+            self.nodes[label] = self.Node(label)
+            self.adjacency_list[label] = []
+
+    def add_edge(self, source, destination, weight):
+        if source not in self.nodes or destination not in self.nodes:
+            raise Exception('Node does not exist')
+        current_source = self.adjacency_list[source]
+        current_source.append(self.Edge(source, destination, weight))
+        current_source.append(self.Edge(destination, source, weight))
+
+        current_destination = self.adjacency_list[destination]
+        current_destination.append(self.Edge(source, destination, weight))
+        current_destination.append(self.Edge(destination, source, weight))
+
+    def print_graph(self):
+        for item in self.adjacency_list:
+            neighbours = self.adjacency_list[item]
+            for neighbour in neighbours:
+                print(neighbour.source, "-->", neighbour.destination, '(', neighbour.weight, ')')
+        print()
+
+graph = WeightedGraph()
+graph.add_node('A')
+graph.add_node('B')
+graph.add_node('C')
+graph.add_node('D')
+graph.add_edge('A', 'B', 3)
+graph.add_edge('A', 'C', 7)
+graph.add_edge('A', 'D', 5)
+graph.add_edge('B', 'D', 1)
+graph.print_graph()
---
+++ Test.py
@@ -0,0 +1,6 @@
+def imprimir(n, m):
+    for i in range(n):
+        for j in range(n, m):
+            suma = i + j
+            print(suma)
+imprimir(5,10)
