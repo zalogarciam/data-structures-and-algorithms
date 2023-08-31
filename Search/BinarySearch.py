70a33656bef9a03f00a4c0d407272a747d858bb3
zalogarciam <gegarciam95@gmail.com>
2023-08-31 18:48:51 -0500

    Binary searcho iterative

--- Search/BinarySearch.py
+++ Search/BinarySearch.py
@@ -1,5 +1,3 @@
-import math
-
 def binary_search_recursive(array, target):
     return binary_search_recursive_(array, target, 0, len(array) - 1)
 
@@ -12,4 +10,19 @@ def binary_search_recursive_(array, target, left, right):
         return binary_search_recursive_(array, target, left, middle - 1)
     return binary_search_recursive_(array, target, middle + 1, right)
 
-print(binary_search_recursive([1,3,5,6,7], 3))
+# print(binary_search_recursive([1,3,5,6,7], 3))
+
+def binary_search(array, target):
+    left = 0 
+    right = len(array) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        if array[middle] == target:
+            return middle
+        if target < array[middle]:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return -1
+
+print(binary_search([1,3,5,6,7], 3))
