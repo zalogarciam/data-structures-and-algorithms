1feb5c62ab4f6f69ccd4aeb54cbf3cd87f04632e
zalogarciam <gegarciam95@gmail.com>
2023-09-19 17:13:51 -0500

    Comment code

--- Sort/InsertionSort.py
+++ Sort/InsertionSort.py
@@ -1,9 +1,9 @@
 def insertion_sort(array):
     for i in range(1, len(array)):
-        current = array[i]
+        current = array[i] # store element 
         j = i - 1
         while j >= 0 and array[j] > current:
-            array[j+1] = array[j]
+            array[j+1] = array[j] # shift elements to the right
             j-= 1
         array[j+1] = current
 
